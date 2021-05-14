8b2a6df570486be33a558a312f65fda65de5b7cd
David Wolff <info@davidwolff.dk>
2021-05-14 22:10:50 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "De Smaa",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
